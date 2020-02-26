e7fb292dee8bfbae235d292f232e6743d154d24d
alexmaclean23 <alexmaclean23@gmail.com>
2020-02-26 11:20:50 -0500

    Update IPsweep.sh

--- IPsweep.sh
+++ IPsweep.sh
@@ -4,11 +4,11 @@
 if [ "$1" == "" ]
 
 then
-echo "Syntax: ./IPsweep.sh {first 3 octets of IP range}"
+    echo "Syntax: ./IPsweep.sh {first 3 octets of IP range}"
 
 else
-for ip in `seq 1 254`; do
-ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    for ip in `seq 1 254`; do
+    ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
 
 fi
